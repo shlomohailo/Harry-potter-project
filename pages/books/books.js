84df2c3b4fea3e98fdb03c6534dcd4d672bd09df
Shlomo Hailo <shlomoohailo@gmail.com>
2022-08-08 16:39:37 +0300

    commit

--- pages/books/books.js
+++ pages/books/books.js
@@ -18,9 +18,9 @@ function photoForCard() {
     counter++;
     return pic;
 }
+let divbooks = document.getElementById("div_books")
 
 async function dataBooks() {
-    let divbooks = document.getElementById("div_books")
 
 
 
@@ -38,10 +38,10 @@ async function dataBooks() {
                     <h5 class="card-title" > ${element["title"]}</h5>
                     <p class="card-text">${element["author"]}</p>
                    
-                    <p class=" card-text" >${element["description"]}</p>
+                    <p  id="p_rweviow" class=" card-text" >${element["description"]}</p>
 
                     
-                    <button type="button" class="btn btn-primary">Buy</button>
+                    <button type="button" class="btn btn-primary" onclick="paymaymnt()">Buy</button>
                     
                    </div>
               
@@ -54,6 +54,84 @@ async function dataBooks() {
 }
 dataBooks()
 
+
+function paymaymnt() {
+
+    divbooks.innerHTML = ""
+
+
+    divbooks.innerHTML = `
+    <div class="row p-5">
+  <div class="col-75">
+    <div class="container">
+      <form action="/action_page.php">
+      
+        <div class="row">
+          <div class="col-50">
+            <h3>Billing Address</h3>
+            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="">
+            <label for="email"><i class="fa fa-envelope"></i> Email</label>
+            <input type="text" id="email" name="email" placeholder="">
+            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+            <input type="text" id="adr" name="address" placeholder="">
+            <label for="city"><i class="fa fa-institution"></i> City</label>
+            <input type="text" id="city" name="city" placeholder="">
+
+            <div class="row">
+              <div class="col-50">
+                <label for="state">State</label>
+                <input type="text" id="state" name="state" placeholder="">
+              </div>
+              <div class="col-50">
+                <label for="zip">Zip</label>
+                <input type="text" id="zip" name="zip" placeholder="">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-50">
+            <h3>Payment</h3>
+            <label for="fname">Accepted Cards</label>
+            <div class="icon-container">
+              <i class="fa fa-cc-visa" style="color:navy;"></i>
+              <i class="fa fa-cc-amex" style="color:blue;"></i>
+              <i class="fa fa-cc-mastercard" style="color:red;"></i>
+              <i class="fa fa-cc-discover" style="color:orange;"></i>
+            </div>
+            <label for="cname">Name on Card</label>
+            <input type="text" id="cname" name="cardname" placeholder="">
+            <label for="ccnum">Credit card number</label>
+            <input type="text" id="ccnum" name="cardnumber" placeholder="">
+            <label for="expmonth">Exp Month</label>
+            <input type="text" id="expmonth" name="expmonth" placeholder="">
+            <div class="row">
+              <div class="col-50">
+                <label for="expyear">Exp Year</label>
+                <input type="text" id="expyear" name="expyear" placeholder="">
+              </div>
+              <div class="col-50">
+                <label for="cvv">CVV</label>
+                <input type="text" id="cvv" name="cvv" placeholder="">
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        <label>
+          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+        </label>
+        <input type="submit" value="Continue to checkout" class="btn">
+      </form>
+    </div>
+  </div>
+  
+    </div>
+  </div>
+</div>
+    
+    `
+}
 
 
 
